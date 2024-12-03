0da3f73d46442563f921be8b8f100ac05610d6a4
Saeed Kiarsi Zadeh <skiarsi66@gmail.com>
2024-12-03 12:59:30 -0600

    change home client color

--- src/app/components/Homeclient.tsx
+++ src/app/components/Homeclient.tsx
@@ -4,6 +4,6 @@ import React from 'react'
 
 export default function Homeclient() {
   return (
-    <p className='block text-2xl'>Home from client</p>
+    <p className='block text-2xl text-red-600'>Home from client</p>
   )
 }
